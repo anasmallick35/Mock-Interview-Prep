64c1a279e00184abbaa4d4f36316e937dc75c81c
Anas Mallick <anasmallick846@gmail.com>
2025-03-14 15:34:25 +0530

    saved

--- src/containers/TakeInterview/index.tsx
+++ src/containers/TakeInterview/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
-import { chatSession } from "@/utils/gemini";
+//import { chatSession } from "@/utils/gemini";
 import TakeInterviewComponent from "@/components/TakeInterview";
 import { useDispatch, useSelector } from "react-redux";
 import { createInterviewStart } from "@/redux/slices/TakeInterviewSlice";
